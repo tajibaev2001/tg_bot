70122da1c88b2f01ddba9dcc7dc05dbef5dba9fe
tajibaev2001 <tajibaev200@gmail.com>
2023-06-06 20:08:46 +0600

    first commit

--- bot.py
+++ bot.py
@@ -6,6 +6,9 @@ from aiogram import Bot, Dispatcher, types
 from conflig import TOKEN
 from aiogram.filters.command import Command
 from aiogram.enums.dice_emoji import DiceEmoji
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from aiogram.types import KeyboardButton
 
 logging.basicConfig(level=logging.INFO)
 
@@ -23,7 +26,53 @@ async def start(message: types.Message):
 
 @dp.message(Command("dice"))
 async def dice(message: types.Message):
-    await message.answer_dice(emoji=DiceEmoji.DART)
+    await message.answer_dice(emoji=DiceEmoji.FOOTBALL)
+
+@dp.message(Command("special_buttonn"))
+async def special_button(massage: types.Message):
+    builder = ReplyKeyboardBuilder()
+    builder.row(
+        types.KeyboardButton(text="Отправка номера", request_contact = True),
+        types.KeyboardButton(text="Отправка гелокации", request_location=True),
+
+    )
+    builder.row(types.KeyboardButton(
+        text="Создать викторину",
+        request_poll = types.KeyboardButtonPollType(type="quiz")
+
+    ))
+
+    await massage.answer(
+        "Выберите дейстия: ",
+        reply_markup=builder.as_markup(resize_keyboard = True)
+    )
+@dp.message(Command("inline_button"))
+async def inline_button(message: types.Message):
+    builder = InlineKeyboardBuilder()
+    builder.row(types.InlineKeyboardButton(
+        text="GitHub", url="https://github.com/"
+    ))
+    builder.row(types.InlineKeyboardButton(
+        text="Telegram",
+        url="tg://resolve?domain=telegram"
+    ))
+    
+    user_id = 1388785377
+    chat_info = await bot.get_chat(user_id)
+    if not chat_info.has_private_forwards:
+        builder.row(types.InlineKeyboardButton(
+            text="Какой-то пользователь",
+            url=f"tg://user?id={user_id}"
+        ))
+    
+    await message.answer(
+        "Выберите ссылку",
+        reply_markup=builder.as_markup(),
+    )
+
+@dp.message(Command('id'))
+async def id(message: types.Message):   
+    await message.answer(message.from_user.id)
 
 @dp.message()
 async def dice(message: types.Message):
@@ -34,3 +83,4 @@ async def main():
     
 if __name__ == "__main__":
     asyncio.run(main())
+
